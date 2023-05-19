a1e0a8eea347e50566b43ec7b891e30b72921665
oscar <opmendoza94@gmail.com>
2023-05-18 20:05:12 -0600

    uso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'uso',
+    loadChildren: () => import('./uso/uso.module').then( m => m.UsoPageModule)
   },
+
 ];
 
 @NgModule({
